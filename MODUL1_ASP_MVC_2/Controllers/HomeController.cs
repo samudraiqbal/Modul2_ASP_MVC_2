bf9e0de0841c75595f0e65ffc40bd8cd1d4c98b3
Samudra~ <Samudra~@SAMUDRA>
2017-01-31 14:46:06 +0700

    Add project files.

---
+++ MODUL1_ASP_MVC_2/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using MODUL1_ASP_MVC_2.Security;
+using System.Web.Mvc;
+
+namespace MODUL1_ASP_MVC_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+}
